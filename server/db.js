b687797aa9062876bac57b68dc771f676510b1e8
Yash-ui-hue <yashvswkra@gmail.com>
2026-05-10 19:54:36 +0530

    fix SSL for supabase connection

--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
 const { Pool } = require('pg');
 // require('dotenv').config();
 
+
 require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 pool.on('error', (err) => {
