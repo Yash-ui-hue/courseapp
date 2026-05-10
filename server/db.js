f19be1b5d4b4b15e48ccd72080516f05d0a884db
Yash-ui-hue <yashvswkra@gmail.com>
2026-05-10 22:49:06 +0530

    db.js updated

--- server/db.js
+++ server/db.js
@@ -1,16 +1,14 @@
 const { Pool } = require('pg');
-// require('dotenv').config();
-
-
 require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  max: 1,
 });
 
 pool.on('error', (err) => {
   console.error('Unexpected DB error:', err);
 });
 
-module.exports = pool;  
+module.exports = pool;
