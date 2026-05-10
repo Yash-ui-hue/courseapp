638a039358147de1c3595a4ed58a983493434037
Yash-ui-hue <yashvswkra@gmail.com>
2026-05-10 22:59:25 +0530

    fix cross-domain cookie for production

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -49,8 +49,8 @@ router.post('/register', [
     // Set HTTP-only cookie
     res.cookie('token', token, {
       httpOnly: true,
-      secure: false,       // set to true in production (HTTPS)
-      sameSite: 'strict',
+      secure: true,       // set to true in production (HTTPS)
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000  // 7 days in ms
     });
 
