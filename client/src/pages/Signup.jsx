273f30e5b35ea2f71df9cc257bcc604311475dab
Yash-ui-hue <yashvswkra@gmail.com>
2026-05-13 12:16:33 +0530

    minor styling changes

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -67,7 +67,8 @@ export default function Signup() {
                 value={form.password} onChange={e => setForm({...form,password:e.target.value})} required/>
             </div>
             <button className="btn btn-primary btn-lg" type="submit" disabled={loading}
-              style={{width:'100%',marginTop:2}}>
+              style={{width:'100%',marginTop:2, background: 'var(--text)',
+                color: 'var(--bg)'}}>
               {loading ? <div className="spinner"/> : 'Create account'}
             </button>
           </form>
